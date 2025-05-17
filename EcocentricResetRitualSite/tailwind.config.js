780213ad19ca8d39b3da031e6a0a43021d741724
alexislindsay <40674693+alexislindsay@users.noreply.github.com>
2025-05-17 23:12:48 +0200

    Update tailwind.config.js

--- EcocentricResetRitualSite/tailwind.config.js
+++ EcocentricResetRitualSite/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [require("@tailwindcss/typography")], // if using .prose
+};
